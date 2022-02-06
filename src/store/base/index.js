9b779ade206528d3339ae2eaed475b07b19fe39b
Yvan Sabay <sabayyvan2018@gmail.com>
2022-02-06 23:43:36 +0800

    fixes for some bugs

--- src/store/base/index.js
+++ src/store/base/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  // baseURL: 'http://127.0.0.1:8000/api'
-  baseURL: 'https://be.lnukiosk.live/api'
+  baseURL: 'http://127.0.0.1:8000/api'
+  // baseURL: 'https://be.lnukiosk.live/api'
 });
 
 export default API;
